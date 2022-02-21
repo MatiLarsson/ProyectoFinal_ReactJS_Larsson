bc63a1a60b0e5e0b36479f413413ff0f4d3ee1d6
Matias Larsson <larssonmts@gmail.com>
2022-02-21 16:44:10 -0300

    Entrega de desafío clase 3

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <h1>Mi Footer</h1>
+            <p>&copy; copyright 2022 Matias Larsson</p>
+            <p>Todos los derechos reservados</p>
+        </footer>
+    )
+}
+
+export default Footer
